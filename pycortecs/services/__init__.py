030d34d4a52e27b53a8d27d6dfbb884de8904f69
max <a01528519@unet.univie.ac.at>
2021-07-28 15:22:31 +0200

    remove yml config

--- pycortecs/services/__init__.py
+++ pycortecs/services/__init__.py
@@ -1,8 +1,13 @@
-import yaml
-
-base_conf = yaml.safe_load(open("./config.yml", 'r'))
-
-CONNECTION_DETAILS = base_conf['server_connection']
-
+CONNECTION_DETAILS = {
+    'base_url': "http://185.232.71.249:8002",
+    'login': "/token",
+    'headers': {
+        'login_header': {
+            "accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"},
+        'token_header': {
+            "accept": "application/json",
+            "authorization": ""
+        }
+    }}
 BASE_URL = CONNECTION_DETAILS['base_url']
-
